3a696dfb31ff3ca934e4ef8e6a575cb4f5bcdaf9
Laketequin1 <Laketequin1@gmail.com>
2023-11-03 02:19:31 +1300

    Formatted into a class, trying making buttons disappear.

--- main.py
+++ main.py
@@ -1,172 +1,211 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QProgressBar, QGraphicsDropShadowEffect
 from PyQt5.QtGui import QPixmap, QIcon, QColor, QCursor
-from PyQt5.QtCore import Qt, QPropertyAnimation
+from PyQt5.QtCore import Qt
 
 ORIGINAL_HEIGHT = 540
 
-def pause():
-    download_button.canceled = True
+def clamp(value):
+    return max(0, min(1, value))
 
-def download():
-    download_button.setStyleSheet(
-        f"""
-            :!hover {{
-                border: 1px solid black;
-                font-size: {round(24 * size_multiplier)}px;
-                font-weight: bold;
-                color: white;
-                background-color: #5076e6;
-                border-radius: 5px;
-            }}
+class GUI:
+    class Download:
+        def __init__(self, app, window, size_multiplier):
+            self.app = app
+            self.window = window
+            self.size_multiplier = size_multiplier
 
-            :hover {{
-                border: 1px solid black;
-                font-size: {round(24 * size_multiplier)}px;
-                font-weight: bold;
-                color: white;
-                background-color: #5c81ed;
-                border-radius: 5px;
-            }}
-        """
-    )
-    download_button.clicked.disconnect(download)
-    download_button.clicked.connect(pause)
-    download_button.setText("Pause")
+            self.setup_ui()
+            self.setup_download_button()
+            self.setup_progress_bar()
 
-    import time
-    for i in range(101):
-        if download_button.canceled:
-            break
-        progress_bar.setValue(i)
-        app.processEvents()
-        time.sleep(0.05)
-    else:
-        download_button.setStyleSheet(
-            f"""
-                border: 1px solid black;
-                font-size: {round(24 * size_multiplier)}px;
-                font-weight: bold;
-                color: white;
-                background-color: #3bc449;
-                border-radius: 5px;
-            """
-        )
-        download_button.clicked.disconnect(pause)
-        download_button.setText("FINISHED!")
-        download_button.setCursor(QCursor(Qt.ArrowCursor))
-        return
-    
-    download_button.setStyleSheet(
-        f"""
-            :!hover {{
-                border: 1px solid black;
-                font-size: {round(24 * size_multiplier)}px;
-                font-weight: bold;
-                color: white;
-                background-color: #1948d1;
-                border-radius: 5px;
-            }}
+        def setup_ui(self):
+            # Set the window icon
+            self.window.setWindowIcon(QIcon("images/icon.ico"))  # Replace with the actual path to your icon file
 
-            :hover {{
-                border: 1px solid black;
-                font-size: {round(24 * size_multiplier)}px;
-                font-weight: bold;
-                color: white;
-                background-color: #2c59de;
-                border-radius: 5px;
-            }}
-        """
-    )
-    download_button.clicked.disconnect(pause)
-    download_button.clicked.connect(download)
-    download_button.canceled = False
-    download_button.setText("Resume")
+            # Get screen dimensions
+            screen_width = self.app.primaryScreen().size().width()
+            screen_height = self.app.primaryScreen().size().height()
+            window_width = screen_width // 2
+            window_height = screen_height // 2
 
-app = QApplication(sys.argv)
-window = QMainWindow()
+            # Set the window title and dimensions
+            self.window.setWindowTitle("Cyclic Warriors Launcher")
+            self.window.setFixedSize(window_width, window_height)
 
-# Set the window icon
-window.setWindowIcon(QIcon("images/icon.ico"))  # Replace with the actual path to your icon file
+            # Create a QLabel for the background image
+            background_label = QLabel(self.window)
+            background_label.setGeometry(0, 0, window_width, window_height)
+            background_image = QPixmap("images/background.jpg")  # Change to your image file path
+            background_image = background_image.scaled(window_width, window_height)
+            background_label.setPixmap(background_image)
 
-# Get screen dimensions
-screen_width = app.primaryScreen().size().width()
-screen_height = app.primaryScreen().size().height()
-window_width = screen_width // 2
-window_height = screen_height // 2
+            # Add a title label with bold text, centered
+            self.title_label = QLabel("Cyclic Warriors Launcher", self.window)
+            self.title_label.setAlignment(Qt.AlignCenter)
+            self.title_label.setGeometry(0, round(window_height * 0.1), window_width, round(60 * self.size_multiplier))
+            self.title_label.setStyleSheet(f"font-size: {round(48 * self.size_multiplier)}px; font-weight: bold; color: white;")
+            shadow = QGraphicsDropShadowEffect()
+            shadow.setBlurRadius(10)
+            shadow.setColor(QColor(0, 0, 0))
+            shadow.setOffset(0, 0)
+            self.title_label.setGraphicsEffect(shadow)
 
-size_multiplier = window_height / ORIGINAL_HEIGHT
+        def setup_download_button(self):
+            # Create a button widget and style it
+            self.download_button = QPushButton("Download", self.window)
+            self.download_button.setGeometry(round(self.window.width() / 2 - round(200 * self.size_multiplier) / 2), int(self.window.height() * 0.42), round(200 * self.size_multiplier), round(50 * self.size_multiplier))
+            self.download_button.clicked.connect(self.download)
+            self.download_button.canceled = False
+            self.download_button.setStyleSheet(
+                f"""
+                :!hover {{
+                    border: 1px solid black;
+                    font-size: {round(24 * self.size_multiplier)}px;
+                    font-weight: bold;
+                    color: white;
+                    background-color: #1948d1;
+                    border-radius: 5px;
+                }}
 
-# Set the window title and dimensions
-window.setWindowTitle("Cyclic Warriors Launcher")
-window.setFixedSize(window_width, window_height)
+                :hover {{
+                    border: 1px solid black;
+                    font-size: {round(24 * self.size_multiplier)}px;
+                    font-weight: bold;
+                    color: white;
+                    background-color: #2c59de;
+                    border-radius: 5px;
+                }}
+                """
+            )
+            self.download_button.setCursor(QCursor(Qt.PointingHandCursor))
+            shadow = QGraphicsDropShadowEffect()
+            shadow.setBlurRadius(10)
+            shadow.setColor(QColor(0, 0, 0))
+            shadow.setOffset(0, 0)
+            self.download_button.setGraphicsEffect(shadow)
 
-# Create a QLabel for the background image
-background_label = QLabel(window)
-background_label.setGeometry(0, 0, window_width, window_height)
-background_image = QPixmap("images/background.jpg")  # Change to your image file path
-background_image = background_image.scaled(window_width, window_height)
-background_label.setPixmap(background_image)
+        def setup_progress_bar(self):
+            # Create a Progressbar widget
+            self.progress_bar = QProgressBar(self.window)
+            self.progress_bar.setGeometry(self.window.width() // 5, int(self.window.height() * 0.55), 3 * self.window.width() // 5, round(30 * self.size_multiplier))
+            self.progress_bar.setStyleSheet(
+                """
+                QProgressBar {
+                    border: 2px solid grey;
+                    border-radius: 5px;
+                    text-align: center;
+                    font-size: 24px;
+                }
+                QProgressBar::chunk {
+                    background-color: #2ad452;
+                    border-radius: 3px;
+                }
+                """
+            )
+            shadow = QGraphicsDropShadowEffect()
+            shadow.setBlurRadius(10)
+            shadow.setColor(QColor(0, 0, 0))
+            shadow.setOffset(0, 0)
+            self.progress_bar.setGraphicsEffect(shadow)
 
-# Add a title label with bold text, centered
-title_label = QLabel("Cyclic Warriors Launcher", window)
-title_label.setAlignment(Qt.AlignCenter)
-title_label.setGeometry(0, round(window_height * 0.1), window_width, round(60 * size_multiplier))
-title_label.setStyleSheet(f"font-size: {round(48 * size_multiplier)}px; font-weight: bold; color: white;")
-shadow = QGraphicsDropShadowEffect()
-shadow.setBlurRadius(10)
-shadow.setColor(QColor(0, 0, 0))
-shadow.setOffset(0, 0)
-title_label.setGraphicsEffect(shadow)
+        def download(self):
+            self.download_button.setStyleSheet(
+                f"""
+                :!hover {{
+                    border: 1px solid black;
+                    font-size: {round(24 * self.size_multiplier)}px;
+                    font-weight: bold;
+                    color: white;
+                    background-color: #3e64d6;
+                    border-radius: 5px;
+                }}
 
-# Create a button widget and style it
-download_button = QPushButton("Download", window)
-download_button.setGeometry(round(window_width / 2 - round(200 * size_multiplier) / 2), int(window_height * 0.4), round(200 * size_multiplier), round(50 * size_multiplier))
-download_button.clicked.connect(download)
-download_button.canceled = False
-download_button.setStyleSheet(
-    f"""
-        :!hover {{
-            border: 1px solid black;
-            font-size: {round(24 * size_multiplier)}px;
-            font-weight: bold;
-            color: white;
-            background-color: #1948d1;
-            border-radius: 5px;
-        }}
+                :hover {{
+                    border: 1px solid black;
+                    font-size: {round(24 * self.size_multiplier)}px;
+                    font-weight: bold;
+                    color: white;
+                    background-color: #5377e0;
+                    border-radius: 5px;
+                }}
+                """
+            )
+            self.download_button.clicked.disconnect(self.download)
+            self.download_button.clicked.connect(self.pause)
+            self.download_button.setText("Pause")
 
-        :hover {{
-            border: 1px solid black;
-            font-size: {round(24 * size_multiplier)}px;
-            font-weight: bold;
-            color: white;
-            background-color: #2c59de;
-            border-radius: 5px;
-        }}
-    """
-)
-download_button.setCursor(QCursor(Qt.PointingHandCursor))
-shadow = QGraphicsDropShadowEffect()
-shadow.setBlurRadius(10)
-shadow.setColor(QColor(0, 0, 0))
-shadow.setOffset(0, 0)
-download_button.setGraphicsEffect(shadow)
+            import time
+            for i in range(1001):
+                if self.download_button.canceled:
+                    break
+                self.progress_bar.setValue(round(i / 10))
+                self.app.processEvents()
+                time.sleep(0.01)
+            else:
+                self.download_button.setStyleSheet(
+                    f"""
+                    border: 1px solid black;
+                    font-size: {round(24 * self.size_multiplier)}px;
+                    font-weight: bold;
+                    color: white;
+                    background-color: #2ad452;
+                    border-radius: 5px;
+                    """
+                )
+                self.download_button.clicked.disconnect(self.pause)
+                self.download_button.setText("FINISHED!")
+                self.download_button.setCursor(QCursor(Qt.ArrowCursor))
+                self.app.processEvents()
+                time.sleep(1)
+                self.close_elements()
+                return
 
-# Create a Progressbar widget
-progress_bar = QProgressBar(window)
-progress_bar.setGeometry(int(window_width / 2 - 100), int(window_height * 0.6), 200, 30)
-progress_bar.setStyleSheet("QProgressBar {border: 2px solid grey; border-radius: 5px;} QProgressBar::chunk {background-color: blue; border-radius: 3px;}")
-shadow = QGraphicsDropShadowEffect()
-shadow.setBlurRadius(10)
-shadow.setColor(QColor(0, 0, 0))
-shadow.setOffset(0, 0)
-progress_bar.setGraphicsEffect(shadow)
+            self.download_button.setStyleSheet(
+                f"""
+                :!hover {{
+                    border: 1px solid black;
+                    font-size: {round(24 * self.size_multiplier)}px;
+                    font-weight: bold;
+                    color: white;
+                    background-color: #1948d1;
+                    border-radius: 5px;
+                }}
 
+                :hover {{
+                    border: 1px solid black;
+                    font-size: {round(24 * self.size_multiplier)}px;
+                    font-weight: bold;
+                    color: white;
+                    background-color: #2c59de;
+                    border-radius: 5px;
+                }}
+                """
+            )
+            self.download_button.clicked.disconnect(self.pause)
+            self.download_button.clicked.connect(self.download)
+            self.download_button.canceled = False
+            self.download_button.setText("Resume")
 
-# Center the window on the screen
-screen_center_x = (screen_width - window_width) // 2
-screen_center_y = (screen_height - window_height) // 2
-window.setGeometry(screen_center_x, screen_center_y, window_width, window_height)
+        def pause(self):
+            self.download_button.canceled = True
 
-window.show()
-sys.exit(app.exec_())
+        def close_elements(self):
+            # Hide or remove the download button, progress bar, and other elements
+            self.download_button.hide()  # Hide the download button
+            self.progress_bar.hide()    # Hide the progress bar
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    size_multiplier = window.height() / ORIGINAL_HEIGHT
+
+    download_manager = GUI.Download(app, window, size_multiplier)
+
+    # Center the window on the screen
+    screen_center_x = (app.primaryScreen().size().width() - window.width()) // 2
+    screen_center_y = (app.primaryScreen().size().height() - window.height()) // 2
+    window.setGeometry(screen_center_x, screen_center_y, window.width(), window.height())
+
+    window.show()
+    sys.exit(app.exec_())
